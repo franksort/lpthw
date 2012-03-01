06f220bb03cb7115f7db3a3c5fee0d81c3d625d1
Frank S <franksort@gmail.com>
2012-03-01 13:58:24 -0800

    Did extra credit for #3

--- ex3.py
+++ ex3.py
@@ -1,23 +1,33 @@
+# Printing a string.
 print "I will now count my chickens:"
 
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+# Printing some strings and the result of some math operations.
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100 - 25 * 3 % 4.0
 
+# Printing a string.
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Printing the result of some math operations.
+print 3 + 2 + 1 - 5 + 4 % 2.0 - 1 / 4.0 + 6
 
+# Printing a string.
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# Printing the result of some math operations.
 print 3 + 2 < 5 - 7
 
+# Printing some strings and the results of some math operations.
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
 
+# Printing a string.
 print "Oh, that's why it's False."
 
+# Printing a string.
 print "How about some more."
 
+# Printing some strings and the results of some math operations.
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
---
+++ ex3_ec3.py
@@ -0,0 +1,7 @@
+# 3. Find something you need to calculate and write
+# a new .py file that does it.
+
+print "Minutes to complete an exercise:", 10
+print "Number of exercises in LPTHW:", 52
+print "Minutes to do this whole tutorial:", 10 * 52
+print "Hours: ", 10 * 52 / 60
