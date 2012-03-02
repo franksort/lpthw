67bc374b4942784ce4645df5a2d45310ef173e8a
Frank S <franksort@gmail.com>
2012-03-02 15:09:34 -0800

    Added some thoughts about print and the use of a comma.

--- ex7.py
+++ ex7.py
@@ -19,9 +19,12 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# Concatenating variables together with the plus operator then printing
-# the comma maybe creates a list of print statements so the newline isn't
-# printed until the end?  I'm not sure.
+# Concatenating variables together with the plus operator then printing.
+# I don't understand how the comma is able to alter the way print operates
+
 # watch that comma at the end.  try removing it to see what happens
+# without the comma, the strings are printed on seperate lines
+# with the comma the strings are printed on the same line but 
+# with a space between them
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
