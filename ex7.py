60bbcce00ba4b49de1c48b541f3e824913b93009
Frank S <franksort@gmail.com>
2012-03-02 15:05:25 -0800

    Completed Ex. 7

---
+++ ex7.py
@@ -0,0 +1,27 @@
+# Simple print statements.
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+# First, "." * 10 creates a string of 10 periods then it's printed
+print "." * 10 # what'd that do?
+
+# Assigning strings to variables.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Concatenating variables together with the plus operator then printing
+# the comma maybe creates a list of print statements so the newline isn't
+# printed until the end?  I'm not sure.
+# watch that comma at the end.  try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
