a037ca2020572c8e0fa296f7c834a0775ca4575a
Frank S <franksort@gmail.com>
2012-03-03 21:19:42 -0800

    Completed Ex. 9 Extra Credit Added some thoughts about triple-quoted strings.

--- ex9.py
+++ ex9.py
@@ -1,11 +1,19 @@
+# The following line is a comment.
 # Here's some new strange stuff, remember type it exactly.
 
+# Assigning strings to variables.
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# Printing a string followed by the string contained in the variable
 print "Here are the days: ", days
 print "Here are the months: ", months
 
+# Not sure of the name of it, but the triple-quoted
+# string is just a way to express a string over multiple
+# lines.  Often used to document modules/functions.
+# Called a docstring in that case.
+# This statement just prints a string spanning multiple lines.
 print """
 There's something going on here.
 With the three double-quotes.
