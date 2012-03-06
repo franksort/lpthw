26f528bcfc4fb1fb66f47be1ecdb01ad3bc94567
Frank S <franksort@gmail.com>
2012-03-06 12:14:44 -0800

    Completed Ex. 17 w/ EC

--- ex17.py
+++ ex17.py
@@ -1,24 +1,6 @@
 from sys import argv
-from os.path import exists
 
-script, from_file, to_file = argv
-
-print "Copying from %s to %s" % (from_file, to_file)
-
-# we could do these two on one line too, how?
-input = open(from_file)
-indata = input.read()
-
-print "The input file is %d bytes long" % len(indata)
-
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
-
-output = open(to_file, 'w')
-output.write(indata)
-
-print "Alright, all done."
-
-output.close()
-input.close()
+# EC 3
+# As a oneliner without file exists checking or closing the
+# files.  Probably bad form here.
+open(argv[2], 'w').write(open(argv[1]).read()))
