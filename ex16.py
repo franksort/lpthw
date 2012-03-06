b48c841c4b4e3c68014d8183585e136aed825693
Frank S <franksort@gmail.com>
2012-03-06 11:37:42 -0800

    Completed Ex. 16 w/ EC 1

---
+++ ex16.py
@@ -0,0 +1,40 @@
+# EC1: I feel I understand it.
+# file.truncate() is not necessary since creating a file object
+# with the write argument wipes the file.
+# From pydoc file:
+#   The file will be created if it doesn't exist
+#   when opened for writing or appending; it will be truncated when
+#   opened for writing.
+from sys import argv
+
+script, filename = argv
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit RETURN."
+
+raw_input("?")
+
+print "Opening the file..."
+target = open(filename, 'w')
+
+print "Truncating the file.  Goodbye!"
+target.truncate()
+
+print "Now I'm going to ask you for three lines."
+
+line1 = raw_input("line 1: ")
+line2 = raw_input("line 2: ")
+line3 = raw_input("line 3: ")
+
+print "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print "And finally, we close it."
+target.close()
