9d922bc7d825b8606103a4e985c6026d3e26610d
Frank S <franksort@gmail.com>
2012-03-05 19:19:17 -0800

    Completed Ex. 12 Extra Credit

--- ex12.py
+++ ex12.py
@@ -1,3 +1,9 @@
+# EC is about pydoc.
+# Interesting that pydoc can spawn a webserver and serve the Python docs.
+# Useful if you're connection is knocked out.
+
+# I looked up some modules with pydoc but didn't read them line for line.
+
 age = raw_input("How old are you? ")
 height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
