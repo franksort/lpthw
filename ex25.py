c984bde96ea63f5c30541b081f25980b49ff091d
Frank S <franksort@gmail.com>
2012-03-12 16:06:32 -0700

    Completed Ex. 25 EC1

--- ex25.py
+++ ex25.py
@@ -1,3 +1,40 @@
+# EC1
+# Line 23-24
+# The print_first_word function takes a list as an argument,
+# pops the first word off it and stores it in a 'word' variable,
+# then prints the contents of the variable.
+# Since Python arguments are passed by reference, sorted_words
+# now contains all the words of the sentence except the first
+# word.
+#
+# Line 25-26
+# print_last_word operates in the same way as print_first_word
+# except it pops and prints the last word in a sentence
+# instead of the first.
+# 
+# Line 27-28
+# Typing sorted_words into the Python shell (is that the real
+# name of it?) just prints the variable's value.  In this case
+# we see the sorted_word list is missing its first and last word
+# due to the print_first_word and print_last_word functions.
+# 
+# Line 29-31
+# Assigns sorted_words to return the value of sort_sentence()
+# which sorts the resulting list from break_words().
+# The value of sorted_words is then displayed.
+# 
+# Line 32-24
+# print_first_and_list calls break_words to split() the string 
+# in sentence into a list of words.  This list is passed into 
+# print_first_word and print_last_word.
+# 
+# Line 35-37
+# print_first_and_last_sorted calls sort_sentence which results
+# in a list of sorted words from the sentence string.  The word
+# list is then passed into print_first_word and print_last_word.
+# 
+# Line 38
+# Control-D to exit the shell.
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
