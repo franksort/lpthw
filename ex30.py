dd2c953e481a6483b2a671f67e37f0a632ded443
Frank S <franksort@gmail.com>
2012-03-13 15:14:59 -0700

    Completed Ex. 30 w/ EC

--- ex30.py
+++ ex30.py
@@ -1,10 +1,22 @@
-people = 30
-cars = 40
-buses = 15
+"""
+EC1
+elif evaluates its expression and executes its body if the preceding
+if-statement does not evaluate to True.
+else executes its body if none of the if- or elif-statements evaluate to True
 
+"""
+# Assigning numbers to variables
+people = 10 
+cars = 30
+buses = 8
 
+# The greater than operator is a comparison operator that compares the 
+# values of cars and people.  The value of cars is greater than the value
+# of people therefore that expression evaluates to True.  The if-statement
+# executes its body since this expression is True.
+# And that explanation is enough for the rest of this program.
 if cars > people:
-    print "We should take the cars."
+    print "We should take the cars." # EC2 will run
 elif cars < people:
     print "We should not take the cars."
 else:
@@ -13,11 +25,16 @@ else:
 if buses > cars:
     print "That's too many buses."
 elif buses < cars:
-    print "Maybe we could take the buses."
+    print "Maybe we could take the buses." # EC2 will run
 else:
     print "We still can't decide."
 
 if people > buses:
-    print "Alright, let's just take the buses."
+    print "Alright, let's just take the buses." # EC2 will run
 else:
     print "Fine, let's stay home then."
+
+
+# EC3
+if cars > people and buses < cars:
+    print 'Whatever man.'
