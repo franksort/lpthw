ddfa5bb9ca67d1c5f098040a976afb9509981ddf
Frank S <franksort@gmail.com>
2012-03-14 15:14:28 -0700

    Completed Ex. 33 w/ All EC I wiped out my changes for EC 3 and 4 but whatever.

--- ex33.py
+++ ex33.py
@@ -1,28 +1,9 @@
+def forfill(stop, step=1):
+    start = 0
+    return range(start, stop, step)
 
-# EC 1, 2
-def fill(upto, step=1):
-    """Fill a list with numbers upto a limit."""
-    i = 0
-    numbers = []
-    while i < upto:
-        numbers.append(i)
-        i += step
-    return numbers
-
-numlist = fill(7, 2) 
+numbers = forfill(30, 3)
 print numlist
-
-i = 0
-numbers = []
-
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
-
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
-
 
 print "The numbers: "
 
