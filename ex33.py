723971c376360f368da656060ab5162d29334915
Frank S <franksort@gmail.com>
2012-03-14 14:58:16 -0700

    Completed Ex. 33 EC 1

--- ex33.py
+++ ex33.py
@@ -1,3 +1,17 @@
+
+# EC 1
+def fill(upto):
+    """Fill a list with numbers upto a limit."""
+    i = 0
+    numbers = []
+    while i < upto:
+        numbers.append(i)
+        i += 1
+    return numbers
+
+numlist = fill(7)
+print numlist
+
 i = 0
 numbers = []
 
