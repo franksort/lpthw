9fbb3cb354a0efafa5453769ed0a1f7f5ceda4f1
Frank S <franksort@gmail.com>
2012-03-14 17:36:53 -0700

    Completed Ex. 35 EC 5 next.isdigit() isn't the best solution for checking that a string is a dollar amount. It will fail if there are commas or periods or dollar signs, for instance.

--- ex35.py
+++ ex35.py
@@ -5,7 +5,7 @@ def gold_room():
     print "This room is full of gold.  How much do you take?"
 
     next = raw_input("> ")
-    if "0" in next or "1" in next:
+    if next.isdigit():
         how_much = int(next)
     else:
         dead("Man, learn to type a number.")
