520066930c8d22648775b6ad3c68aaf0a2ec1c75
Frank S <franksort@gmail.com>
2012-03-21 16:54:04 -0700

    Completed Ex. 36 This program is laughable.  If-statement hell really. What would really help is a way to save state of the player's inventory and what's in each room at any given time.  What's the best way to do that?

--- ex36.py
+++ ex36.py
@@ -1,55 +1,110 @@
-class Chef:
+def dungeon(inventory):
+    print "You are now in the dungeon."
 
-    def __init__(self, name):
-        self.name = 'Chef %s' % name
-        self.basket = []
+    print "A wild dragon appears!"
+    print "He is blocking a pile of gold."
 
-class Refrigerator:
+    print "What do you do?"
+    cmdlist = ["punch", "fight", "use mace", "use hammer", "back"]
+    cmd = getcmd(cmdlist)
 
-    def __init__(self, chef):
-        self.chef = chef
-        self.items = ['bell pepper', 'onion', 'lettuce']
+    if cmd == "punch" or cmd == "fight":
+        print "You try to fight the dragon but he eats you."
+        print "The end."
+        exit(0)
+    elif cmd == "use hammer":
+        print "You laughably try to use a hammer to fight a dragon."
+        print "He breathes fire and turns you into ash."
+        print "The end."
+        exit(0)
+    elif cmd == "use mace":
+        if "mace" in inventory:
+            print "You club the dragon with the mace, killing him."
+            print "You take some gold and live a happy life."
+            exit(0)
+        else:
+            print "You don't have a mace!"
+            dungeon(inventory)
+    elif cmd == "back":
+        start(inventory) 
 
-    def look(self):
-        print "You look in the refrigerator and see:"
-        for item in self.items:
-            print '->', item
+def armory(inventory):
+    print "You are now in the armory."
+    print "Go back or goto the workbench."
 
-if __name__ == '__main__':
+    cmdlist = ["back", "workbench"]
+    cmd = getcmd(cmdlist)
 
-    print "What's your name?"
-    name = raw_input('> ')
+    if cmd == "back":
+        start(inventory)
+    elif cmd == "workbench":
+        workbench(inventory)
+    else:
+        print "Something went wrong." # this shouldn't execute
+        exit(0)
 
-    chef = Chef(name)
-    refrigerator = Refrigerator(chef)
+def workbench(inventory, items=["nails", "bat"]):
+    print "You are now at the workbench."
+    print "On the workbench you see:"
+    if len(items) > 0:
+        for item in items:
+            print "-> %s" % item
+    else:
+        print "Nothing."
+    print "Try to make something or go back."
 
-    refrigerator.look()
+    cmdlist = ["use hammer", "take mace", "back"]
+    cmd = getcmd(cmdlist)
 
-    choice = raw_input('-> ')
-    if choice.lower() == 'take onion':
-        refrigerator.take('onion')  
-        chef.basket.append('onion')
-"""
+    if cmd == "use hammer":
+        print "You hammer the nails into the bat and create a mace!"
+        items = ['mace']
+        workbench(inventory, items)
+    elif cmd == "take mace" and "mace" in items:
+        if "mace" in inventory:
+            print "You already have a mace."
+        else:
+            print "You have acquired a mace!"
+            inventory.append("mace")
+            items = []
+        workbench(inventory, items)
+    elif cmd == "back":
+        armory(inventory)
+    else:
+        print "Something went wrong." # this shouldn't execute
+        exit(0);
 
-    kitchen = Kitchen(chef)
-    bowl = Bowl(chef)
-    cutting_board = Cutting_Board(chef, bowl)
- 
-    print 'Actions are take, chop, open, close, and eat.'
+def start(inventory):
+    print "You are now in the start room."
+    print "Go left to dungeon or right to armory?"
+    cmdlist = ["left", "right"]
+    cmd = getcmd(cmdlist)
 
-    kitchen.look()
+    if cmd == "left":
+        dungeon(inventory)
+    elif cmd == "right":
+        armory(inventory)
+    else:
+        print "Something went wrong." # this shouldn't execute
+        exit(0);
 
-  
-    basket_items = []
-    kitchen_items = ['knife']
-    refrigerator_items = ['bell pepper',
-                          'lettuce',
-                          'onion'
-                          'dressing']
+def getcmd(cmdlist):
+    cmd = raw_input("> ")
+    if cmd in cmdlist:
+        return cmd
+    elif cmd == "quit":
+        print "Thanks for playing."
+        exit(0)
+    elif cmd == "inventory":
+        print "Your inventory contains:"
+        for item in inventory:
+            print "--> %s" % item
+        return getcmd(cmdlist)
+    else:
+        print "Command not recognized.  Try again."
+        print "Type quit to exit or inventory to view items."
+        return getcmd(cmdlist)
 
- 
-    inventory = Inventory()
-    inventory.add("bell pepper")
-    inventory.view()
-
-"""
+if __name__ == "__main__":
+    inventory = ["hammer"]
+    start(inventory)
