7197c6a17eb4741f16ab586935461dc15e82cba1
Frank S <franksort@gmail.com>
2012-03-23 21:38:08 -0700

    Initial add.  Much to do in this file.

---
+++ ex37/operators.py
@@ -0,0 +1,37 @@
+"""
+Python Operators
+
++             plus
+-             minus
+*             multiply
+**          
+/
+//
+%             modulus
+<             less than
+>             greater than
+<=            less than or equal
+>=            greater than or equal
+==            equality
+!=            inequality
+<>            inequality (deprecated)
+()            open paren, close paren
+[]            open bracket, close bracket
+{}            open brace, close brace
+@             at
+,             comma
+:             colon
+.             period, dot
+=             equal, assignment
+;             semi-colon
++=            increment
+-=            decrement
+*=            multiply equals?
+/=            divide equals?
+//=
+%=            modulus equals?
+**=
+
+TODO: Define what I don't know,
+      do an example for each.
+"""
