4ed721e21c65b2d52929a2e77493518be09341ba
Frank S <franksort@gmail.com>
2012-03-27 15:53:55 -0700

    Datatypes w/o examples.

---
+++ ex37/datatypes.py
@@ -0,0 +1,11 @@
+"""
+Data Types
+
+True          Boolean True
+False         Boolean False
+None          None type
+strings       "blah", 'whatever'
+numbers       3, 3e10, -3
+floats        3.0, -4.3
+lists         [1, 2, 3, 4], ["something", "else"]
+"""
